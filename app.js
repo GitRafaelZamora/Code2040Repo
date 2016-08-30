184273d8b307131f0d75fd791750d4f6abaa06f1
Rafael Zamora <rafaelzamora.me@gmail.com>
2016-08-30 14:30:02 -0700

    finished step5 challenge

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var request = require("request"),
-    bodyParser = require('body-parser'),
-    reverse = require('reverse-string');
+var request = require('request');
+var	express = require('express');
+var bodyParser = require('body-parser');
+var reverse = require('reverse-string');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var github = 'https://github.com/GitRafaelZamora/Code2040Repo.git',
 	API_KEY = '88e4941e9c84e7e092278e5b6dd24d9c',
@@ -40,11 +46,9 @@ var github = 'https://github.com/GitRafaelZamora/Code2040Repo.git',
 			var jsonBody = JSON.parse(body),
 				needle   = jsonBody.needle,
 				haystack = jsonBody.haystack;
-			// console.log(needle);
-			// console.log(haystack);
+
 			haystack.forEach(function(element, index, array){
 				if(element === needle){
-					//console.log("Match! " + element + " = " + needle + index);
 					jsonDictionary.needle = index;
 					request.post({url:'http://challenge.code2040.org/api/haystack/validate', 
 						form: jsonDictionary}, 
@@ -57,34 +61,21 @@ var github = 'https://github.com/GitRafaelZamora/Code2040Repo.git',
 		});
 
  	// Step 4
-	// Great job -- but we’re not done with collections.
-	// In this challenge, the API is going to give you another dictionary. The first value, prefix, is a string. The second value, array, is an array of strings. Your job is to return an array containing only the strings that do not start with that prefix.
-	// Note: The strings in your array should be in the same order as in the original array.
-	// POST your token here:
-	// http://challenge.code2040.org/api/prefix
-	// Once you’ve built your array, POST a dictionary here:
-	// http://challenge.code2040.org/api/prefix/validate
-	// Use the key token for your token.
-	// Use the key array for your array.
-	// Hint: You’ll need a little string-fu to complete this challenge. But rest assured: comparing the beginnings of strings is a common task. Your platform’s standard libraries might even have some code to help you do this.
  	request.post({url:'http://challenge.code2040.org/api/prefix', 
 		form: jsonDictionary}, 
 		function(error,response,body) {
-			// console.log(body);
-			var jsonBody = JSON.parse(body),
-				prefix = jsonBody.prefix,
-				array = jsonBody.array,
-				missMatch = Array();
-				
-			console.log(jsonBody);
-
+			var jsonBody = JSON.parse(body);
+			var	prefix = jsonBody.prefix;
+			var	array = jsonBody.array;
+			var	missMatch = [];
+	
 			for (var i = 0; i < array.length; i++) {
 				if (prefix !== array[i].substring(0,4)) {
 					missMatch.push(array[i]);
 				}
 			}
 
-			console.log(jsonBody)
+			jsonDictionary.array = missMatch;
 
 			request.post({url:'http://challenge.code2040.org/api/prefix/validate', 
 				form: jsonDictionary}, 
@@ -92,5 +83,34 @@ var github = 'https://github.com/GitRafaelZamora/Code2040Repo.git',
 					console.log(body);
 		 		}
 		 	);
- 		}
- 	);
+ 		});
+
+ 	// Step 5
+ 	request.post({url:'http://challenge.code2040.org/api/dating',
+ 		form: jsonDictionary},
+ 		function(error,response,body) {
+ 			var jsonBody = JSON.parse(body);
+ 			var dateString = jsonBody.datestamp;
+ 			var interval = jsonBody.interval;
+
+ 			var seconds = (Date.parse(dateString) / 1000) + interval;
+
+ 			var milliseconds = seconds * 1000;
+
+ 			var date = new Date(milliseconds).toISOString();
+ 			date = date.substring(0,19) + "Z";
+
+			jsonDictionary.datestamp = date;
+
+ 		request.post({url: 'http://challenge.code2040.org/api/dating/validate',
+ 			form: jsonDictionary}, 
+ 			function(error, response, body){
+ 				console.log(body);
+ 			});
+ 	});
+
+
+
+
+
+
