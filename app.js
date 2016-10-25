2287757e7ec98ca28521287c316c6955a1021eeb
Rafael Zamora <rafaelzamora.me@gmail.com>
2016-10-24 18:09:37 -0700

    completed step 4

--- app.js
+++ app.js
@@ -1,92 +1,93 @@
-var request = require('request');
-var	express = require('express');
-var bodyParser = require('body-parser');
-var reverse = require('reverse-string');
+var request = require("request");
+var http = require("http");
+var reverse = require("reverse-string");
 
-var app = express();
+var github = "https://github.com/GitRafaelZamora/Code2040Repo.git";
+var	API_KEY = "88e4941e9c84e7e092278e5b6dd24d9c";
+var	jsonDictionary = {
+        "token"   : API_KEY,
+        "github"  : github
+    };
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-var github = 'https://github.com/GitRafaelZamora/Code2040Repo.git',
-	API_KEY = '88e4941e9c84e7e092278e5b6dd24d9c',
-	jsonDictionary = {
-		'token'   : API_KEY,
-		'github'  : github
-	};
-
-	//Step 1
-	request.post({url:'http://challenge.code2040.org/api/register', 
-		form: jsonDictionary}, 
-		function(error,response,body) {
-			console.log(body);
- 		}
- 	);
+//	Step 1
+    request.post({url:"http://challenge.code2040.org/api/register", 
+	    form: jsonDictionary}, 
+	    function(error,response,body) {
+		    console.log(body);
+        }
+    );
 
  	//Step 2
-	request.post({url:'http://challenge.code2040.org/api/reverse', 
-		form: jsonDictionary}, 
-		function(error,response,body) {
-			//console.log(body);
-			var revBody = reverse(body);
-			//console.log(revBody);
-			jsonDictionary.string = revBody;
-			request.post({url:'http://challenge.code2040.org/api/reverse/validate',
-				form: jsonDictionary},
-				function(error,response,body){
-					console.log(body);
-				});
- 		}
- 	);
+    request.post({url:"http://challenge.code2040.org/api/reverse", 
+        form: jsonDictionary}, 
+        function(error,response,body) {
+            var revBody = reverse(body);
+            jsonDictionary.string = revBody;
+            request.post({url:"http://challenge.code2040.org/api/reverse/validate",
+                form: jsonDictionary},
+                function(error,response,body){
+                    console.log(body);
+                });
+        }
+    );
 
  	//Step 3
- 	request.post({url:'http://challenge.code2040.org/api/haystack',
-		form: jsonDictionary},
-		function(error,response,body){
-			var jsonBody = JSON.parse(body),
-				needle   = jsonBody.needle,
-				haystack = jsonBody.haystack;
+    request.post({url:"http://challenge.code2040.org/api/haystack",
+        form: jsonDictionary},
+        function(error,response,body){
+            var jsonBody = JSON.parse(body);
+            var needle   = jsonBody.needle;
+            var haystack = jsonBody.haystack;
+            haystack.forEach(function(element, index, array){
+                if(element === needle){
+                    jsonDictionary.needle = index;
+                    request.post({url:"http://challenge.code2040.org/api/haystack/validate", 
+    	                form: jsonDictionary}, 
+    	                function(error,response,body) {
+    	                	console.log(body);
+                        }
+    			 	);
+                }
+            })
+        });
 
-			haystack.forEach(function(element, index, array){
-				if(element === needle){
-					jsonDictionary.needle = index;
-					request.post({url:'http://challenge.code2040.org/api/haystack/validate', 
-						form: jsonDictionary}, 
-						function(error,response,body) {
-							console.log(body);
-				 		}
-				 	);
-				}
-			})
-		});
 
- 	// Step 4
- 	request.post({url:'http://challenge.code2040.org/api/prefix', 
-		form: jsonDictionary}, 
+
+//	Step 4
+ 	request.post("http://challenge.code2040.org/api/prefix", 
+		{json: jsonDictionary}, 
 		function(error,response,body) {
-			var jsonBody = JSON.parse(body);
-			var	prefix = jsonBody.prefix;
-			var	array = jsonBody.array;
-			var	missMatch = [];
-	
-			for (var i = 0; i < array.length; i++) {
-				if (prefix !== array[i].substring(0,4)) {
-					missMatch.push(array[i]);
-				}
-			}
+      if (!error && response.statusCode == 200) {
+        var jsonBody = JSON.stringify(body);
+        jsonBody = JSON.parse(jsonBody);
+        var prefix = jsonBody.prefix;
+        var array = jsonBody.array;
+        var missMatch = [];
 
-			jsonDictionary.array = missMatch;
+        array.forEach(function(element, index, array){
+          if (prefix !== element.substring(0,4)) {
+            missMatch.push(element);
+          }
+        })
 
-			request.post({url:'http://challenge.code2040.org/api/prefix/validate', 
-				form: jsonDictionary}, 
-				function(error,response,body) {
-					console.log(body);
-		 		}
-		 	);
- 		});
+        jsonDictionary.array = missMatch;
+        //console.log(jsonDictionary);
+
+        request.post("http://challenge.code2040.org/api/prefix/validate", 
+          {json: jsonDictionary}, 
+          function(error,response,body) {
+            if (!error && response.statusCode == 200){
+              console.log(body);
+            }
+          }
+        )
+      } else {
+        console.log(error);
+      }
+ 	});
 
  	// Step 5
- 	request.post({url:'http://challenge.code2040.org/api/dating',
+ 	request.post({url:"http://challenge.code2040.org/api/dating",
  		form: jsonDictionary},
  		function(error,response,body) {
  			var jsonBody = JSON.parse(body);
@@ -102,12 +103,13 @@ var github = 'https://github.com/GitRafaelZamora/Code2040Repo.git',
 
 			jsonDictionary.datestamp = date;
 
- 		request.post({url: 'http://challenge.code2040.org/api/dating/validate',
+ 		request.post({url: "http://challenge.code2040.org/api/dating/validate",
  			form: jsonDictionary}, 
  			function(error, response, body){
  				console.log(body);
  			});
  	});
+
 
 
 
